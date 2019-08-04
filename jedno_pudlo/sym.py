fff2a828a3f959a6c32c35b4a6175426b3edc9bf
piotrek <piotrpiekos@gmail.com>
2019-08-04 09:28:59 +0200

    updated birth death process simulation to n boxes

--- jedno_pudlo/sym.py
+++ jedno_pudlo/sym.py
@@ -2,18 +2,18 @@ import numpy as np
 import pandas as pd
 
 
-def simulate_birth_death_times(t, a_N, a_S):
+def simulate_birth_death_times(t, a_N, a_Ss):
     """
     Simulates the times of birth and death
     :param t: time interval on which process occurs
     :param a_N: rate of a birth poisson process
-    :param a_S: parameter of exponential distribution for survival time
+    :param a_Ss: array of parameters of exponential distribution for survival time
     :return: births, deaths - vectors of time someone was born or died
     """
     N = np.random.poisson(t * a_N)
     births = np.random.uniform(0, t, N)
-    live_times = np.random.exponential(1 / a_S, N)
-    deaths = births + live_times
+    live_times_arr = [np.random.exponential(1 / a_S, N) for a_S in a_Ss]
+    deaths = [births + live_times for live_times in live_times_arr]
     return births, deaths
 
 
@@ -25,7 +25,8 @@ def simulate_birth_death_process(t, a_N, a_S):
     :param a_S: parameter of exponential distribution for survival time
     :return: births, deaths - vectors of time someone was born or died
     """
-    births, deaths = simulate_birth_death_times(t, a_N, a_S)
+    births, deaths = simulate_birth_death_times(t, a_N, [a_S])
+    deaths = deaths[0]
     df_births = pd.DataFrame({'time': births})
     df_births['type'] = 'birth'
 
